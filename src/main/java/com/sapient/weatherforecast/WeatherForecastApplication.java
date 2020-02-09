865cd08da855a8921d8d3566e579f4eae6888e65
PSEH-9 <40661616+PSEH-9@users.noreply.github.com>
2020-02-09 13:57:03 +0530

    Inital Commit
    Initial Commit

---
+++ src/main/java/com/sapient/weatherforecast/WeatherForecastApplication.java
@@ -0,0 +1,11 @@
+package com.sapient.weatherforecast;
+
+
+@SpringBootApplication
+public class WeatherForecastApplication 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(WeatherForecastApplication.class, args)
+    }
+}
